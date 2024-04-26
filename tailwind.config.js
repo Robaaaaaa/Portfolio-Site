2427e4c79c4ffb04fbd91852e3772a14a2f9b067
Robaaa <linkmash030@gmail.com>
2024-04-26 09:49:35 +0300

    Initialized Rpository

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,49 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./build/*.html","./build/js/*.js"],
+  // Add the folders and files from your templates
+  content: ["./layouts/**/*.html", "./content/**/*.md", "./content/**/*.html", "./src/**/*.js"],
+
+  // Make sure to safelist these classes when using purge
+  safelist: [
+    'w-64',
+    'w-1/2',
+    'rounded-l-lg',
+    'rounded-r-lg',
+    'bg-gray-200',
+    'grid-cols-4',
+    'grid-cols-7',
+    'h-6',
+    'leading-6',
+    'h-9',
+    'leading-9',
+    'shadow-lg'
+  ],
+
+  // Enable dark mode via class strategy
+  darkMode: 'class',
+
+  // Extend or override default theme values
   theme: {
-    extend: {
-      screens: {
-        'widescreen':{'raw': '(min-aspect-ratio: 66/100)'},
-        'tallscreen':{'raw': '(max-aspect-ratio: 65/100)'},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      blue: {
+        light: '#85d7ff',
+        DEFAULT: '#1fb6ff',
+        dark: '#009eeb',
       },
-      keyframes: {
-        'open-menu':{
-          '0%': {transform: 'scaleY(0)'},
-          '80%': {transform: 'scaleY(1.2)'},
-          '100%': {transform: 'scaleY(1)'},
-        },
+      pink: {
+        light: '#ff7ce5',
+        DEFAULT: '#ff49db',
+        dark: '#ff16d1',
       },
-      animation: {
-        'open-menu': 'open-menu 0.5s ease-in-out forwards',
+      gray: {
+        darkest: '#1f2d3d',
+        dark: '#3c4858',
+        DEFAULT: '#c0ccda',
+        light: '#e0e6ed',
+        lightest: '#f9fafc',
       }
-    },
-  },
-  plugins: [],
+    }
+  }
 }
+
